fd040feafcd23b66d238175030d5726f8cea8928
Uttreshwar Nakhate <you@example.com>
2025-06-18 15:00:10 +0530

    created modules and services

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function startApplication () {
+  const APP_PORT=3333
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(APP_PORT);
 }
-bootstrap();
+void startApplication();
